d987a3c9688dc91d500d2ef28da815e4c998e602
suzuki <silverface8888@gmail.com>
2023-06-06 19:04:59 +0900

    settlement(決済用)タブ作成

---
+++ server/laravel/resources/js/Pages/Settlement/Edit.vue
@@ -0,0 +1,37 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    mustVerifyEmail: {
+        type: Boolean,
+    },
+    status: {
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <Head title="Settlement" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Settlement</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+
+                </div>
+
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    
+                </div>
+
+
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
